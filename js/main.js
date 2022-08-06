ca57812fbb398a0001206490f3dccc9ec5cfff4e
Eduard Bogdanov <info@wedjat.ru>
2022-08-06 16:43:56 +0300

    added main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,79 @@
-console.log('test');
+// let someStringWithNumber = "42";
+// someStringWithNumber = +someStringWithNumber;
+// console.log(typeof someStringWithNumber);
+
+//let defaultWellcome = Symbol("здравствуй, дружочек... давай поиграем.....?");
+//console.log(defaultWellcome.description);
+
+//let defaultGoodBye = Symbol("не уходи... ты же об этом пожалеешь.....");
+//console.log(defaultGoodBye.description);
+
+// let price = 5454;
+// let clearPrice = parseInt(price);
+// console.log(typeof clearPrice);
+// console.log(isNaN(clearPrice));
+
+// if 
+// (isNaN(clearPrice) === true ) {
+// 	console.log("Не число");
+// }
+// else if 
+// (clearPrice === 0 || clearPrice < 0) {
+// 	console.log("Число не корректное");
+// }
+// else {
+// 	console.log(clearPrice);
+// };
+
+// console.log(typeof ("4" + +"2"));
+// console.log(typeof "42");
+// console.log("4" - +"2");
+// console.log("4" / +"2");
+// console.log("4" % +"2");
+// console.log("4" ** +"2");
+// console.log("4" * +"2");
+// console.log(NaN + NaN);
+// console.log(5454 + NaN);
+// console.log(typeof(0 + NaN));
+// console.log( + NaN);
+// console.log("fefgwsgwerg" + NaN);
+// console.log(true + NaN);
+// console.log(false + NaN);
+
+// console.log(typeof("4" + +"2"));
+
+
+// let word_1 = "electrocar";
+// let word_2 = "motobyke"
+// console.log(word_1 + word_1.length);
+// console.log(word_2 + word_2.length);
+
+// let compareWord = (word_1.length > word_2.length) ? word_1 : word_2;
+// console.log(compareWord);
+
+// let compareWord 
+// = word_1 > word_2
+// ? word_1
+// 	: word_2 > word_1
+// 	? word_2
+// console.log(compareWord);
+
+
+// let a = 1;
+// console.log(a += 2); // a == 3
+// console.log(a *= 3); // a == 9
+// console.log(a **= 2); // a == 81 (000...001010001)
+// console.log(a >>= 4); // a == 5 (000...000000101)
+
+// console.log(Math.round(Math.random() * (10 ** 10)));
+// console.log(Math.random()*10);
+
+// console.log(!!"false"); // true
+// console.log(typeof Infinity ); // false
+// const a = 10;
+// const b = 10;
+// console.log("12" / 0);
+
+console.log(0 / Infinity);
+console.log(Infinity / Infinity);
+console.log(Infinity / 0);
